b1f4cb1ce408a7e51b19e572e20a58aac28c235d
ZHANG QIANG <tyokyo320@gmail.com>
2019-07-28 21:59:10 +0900

    update

--- sort_algorithm.c
+++ sort_algorithm.c
@@ -214,16 +214,16 @@ void heapSort(int arr[], int n)
 { 
     // Build heap (rearrange array) 
     for (int i = n / 2 - 1; i >= 0; i--) 
-        heapify(arr, n, i); 
+        heapify(arr, n, i);     // Creates a Max heap
   
     // One by one extract an element from heap 
     for (int i = n-1; i >= 0; i--) 
     { 
         // Move current root to end 
-        swap(&arr[0], &arr[i]); 
+        swap(&arr[0], &arr[i]);     // Swaps first and last node
   
         // call max heapify on the reduced heap 
-        heapify(arr, i, 0); 
+        heapify(arr, i, 0);     // Creates max heap on reduced array
     } 
 }
 
