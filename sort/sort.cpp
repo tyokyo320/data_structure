55457b64d5b02feb13603c066471f796b95a4ba6
zhangqiang <tyokyo320@gmail.com>
2021-03-29 22:23:55 +0900

    sort finished

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -2,21 +2,33 @@
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
+
 using namespace std;
 
 void swap(int *x, int *y);
 void printArray(int arr[], int size);
+
+// Time Complexity: O(n*2)
 void insertSort(int arr[], int n);
-void shellSort(int arr[], int n);
 void selectionSort(int arr[], int n);
 void bubbleSort(int arr[], int n);
+
+void shellSort(int arr[], int n);
+
 void heapify(int arr[], int n, int i);
 void heapSort(int arr[], int n);
+
 int partition(int arr[], int low, int high);
 void quickSort(int arr[], int low, int high);
+
 void merge(int arr[], int l, int m, int r);
 void mergeSort(int arr[], int l, int r);
+
 void bucketSort(float arr[], int n);
+
+int getMax(int arr[], int n);
+void countSort(int arr[], int n, int exp);
+void radixSort(int arr[], int n);
 
 int main(int argc, char const *argv[])
 {
@@ -36,6 +48,7 @@ int main(int argc, char const *argv[])
     // cout << "Sorted array is \n";
     // for (int i = 0; i < length; i++)
     //     cout << arr[i] << " ";
+    radixSort(arr, length);
 
     printf("Sorted array: \n");
     printArray(arr, length);
@@ -307,4 +320,57 @@ void bucketSort(float arr[], int n)
     for (int i = 0; i < n; i++)
         for (int j = 0; j < b[i].size(); j++)
             arr[index++] = b[i][j];
+}
+
+// A utility function to get maximum value in arr[]
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+// A function to do counting sort of arr[] according to
+// the digit represented by exp.
+void countSort(int arr[], int n, int exp)
+{
+    int output[n]; // output array
+    int count[10] = {0};
+
+    // Store count of occurrences in count[]
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    // Change count[i] so that count[i] now contains actual
+    // position of this digit in output[]
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    // Build the output array
+    for (int i = n - 1; i >= 0; i--)
+    {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    // Copy the output array to arr[], so that arr[] now
+    // contains sorted numbers according to current digit
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+// The main function to that sorts arr[] of size n using
+// Radix Sort
+void radixSort(int arr[], int n)
+{
+    // Find the maximum number to know number of digits
+    int m = getMax(arr, n);
+
+    // Do counting sort for every digit. Note that instead
+    // of passing digit number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
 }
